45a8e438f36778ef2fcbd29dc5b71d3a6e1aee78
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-02 16:37:19 +0200

    --inserita variabile var trovato per visualizzare correttamente il messaggio della presenza o non presenza dell'email inserita da utente..

--- EMAIL/mail.js
+++ EMAIL/mail.js
@@ -1,17 +1,34 @@
 // creo lista email che hanno accesso
 var listaemail;
-listaemail=["miaemaillibero.it","miaemailgmail.it","miaemailgmail.com","miaemailhotmail.it","miaemailhotmail.com"]
+listaemail = ["miaemaillibero.it","miaemailgmail.it","miaemailgmail.com","miaemailhotmail.it","miaemailhotmail.com"]
 console.log(listaemail);
 //chiedo a utente di inserire la sua email
 
 var emailutente;
-emailutente=prompt("inserisci la tua email");
+emailutente = prompt("inserisci la tua email");
 console.log(emailutente);
 
 //leggo i valori contenuti nell'array listaemail e controllo se è presente quella inserita dall'utente
-for(var i=0; i < listaemail.length; i++){
+var trovato;
+trovato = false;
+for(var i = 0; i < listaemail.length; i++){
   console.log(listaemail[i]);
   if (listaemail[i] === emailutente){
-    alert("sei stato invitato");
+    trovato = true;
   }
 }
+if (trovato == true){
+  alert("presente")
+}else{
+  alert("Non presente")
+}
+// altro metodo
+// var trovato;
+// trovato = "Non presente";
+// for(var i = 0; i < listaemail.length; i++){
+//   console.log(listaemail[i]);
+//   if (listaemail[i] === emailutente){
+//     trovato = "presente";
+//   }
+// }
+// alert(trovato);
